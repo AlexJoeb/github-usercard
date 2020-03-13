6d5a324e88b11b227beb54774dac756a92807e91
Alexander Besse <alexanderjoeb@hotmail.com>
2020-03-13 10:15:52 -0500

    Finished.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,14 @@
+const cards = document.querySelector(".cards");
+
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+
+axios.get(`https://api.github.com/users/alexjoeb`).then(({ data }) => {
+    console.log(data);
+    cards.append(template(data));
+});
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +31,18 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+axios
+    .get("https://api.github.com/users/alexjoeb/followers")
+    .then(({ data }) => {
+        data.forEach(item => {
+            axios
+                .get(`https://api.github.com/users/${item.login}`)
+                .then(({ data }) => {
+                    cards.append(template(data));
+                });
+        });
+    })
+    .catch(err => console.log(err));
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,3 +71,65 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+const element = type => document.createElement(type);
+
+// ! AC = Add Class
+const ac = (e, c) => {
+    e.classList.add(c);
+};
+
+const template = data => {
+    // Define Elements
+    const card = element("div"),
+        cardInfo = element("div"),
+        img = element("img");
+
+    // Add Class Names
+    ac(card, "card");
+    ac(cardInfo, "card-info");
+
+    // Setting content
+    img.src = data.avatar_url; // ! Image of URL for user.
+
+    // Appending
+    card.append(img);
+    card.append(cardInfo);
+
+    // Define Elements
+    const h3 = element("h3"),
+        username = element("p"),
+        location = element("p"),
+        profile = element("p"),
+        a = element("a"),
+        followers = element("p"),
+        following = element("p"),
+        bio = element("p");
+
+    // Add Class Names
+    ac(h3, "name");
+    ac(username, "username");
+
+    // Setting content
+    h3.textContent = data.login;
+    location.textContent = data.location || "It's a secret.";
+    profile.textContent = "Profile: ";
+    a.href = data.html_url;
+    a.textContent = data.html_url;
+    followers.textContent = `Followers: ${data.followers}`;
+    following.textContent = `Following: ${data.following}`;
+    bio.textContent = `Bio: ${data.bio ||
+        "I haven't written my really awesome bio yet."}`;
+
+    // Appending
+    cardInfo.append(h3);
+    cardInfo.append(username);
+    cardInfo.append(location);
+    cardInfo.append(profile);
+    profile.append(a);
+    cardInfo.append(followers);
+    cardInfo.append(following);
+    cardInfo.append(bio);
+
+    return card;
+};
